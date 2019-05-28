99f4302a27155f3f71a8ed46e2f8e4645dea26ad
anupkhadka <akhadka.dev@gmail.com>
2019-05-28 15:24:50 -0400

    Create all the required directories and files. Files are empty right now.

--- lib/sinatra_new/file_generator.rb
+++ lib/sinatra_new/file_generator.rb
@@ -1,6 +1,40 @@
+require 'fileutils'
 class SinatraNew::FileGenerator
 
     def self.generate_files(app_name)
+      FileUtils.mkdir(app_name)
+        # app
+      FileUtils.mkdir("#{app_name}/app")
+        # app/controllers
+      FileUtils.mkdir("#{app_name}/app/controllers")
+      FileUtils.touch("#{app_name}/app/controllers/application_controller.rb")
+
+        # app/models
+      FileUtils.mkdir("#{app_name}/app/models")
+
+        # app/views
+      FileUtils.mkdir("#{app_name}/app/views")
+      FileUtils.touch("#{app_name}/app/views/layout.erb")
+      FileUtils.touch("#{app_name}/app/views/index.erb")
+
+        # app/config
+      FileUtils.mkdir("#{app_name}/app/config")
+      FileUtils.touch("#{app_name}/app/config/environment.rb")
+
+        # app/db
+      FileUtils.mkdir("#{app_name}/app/db")
+      FileUtils.mkdir("#{app_name}/app/db/migrate")
+
+        # config.ru
+      FileUtils.touch("#{app_name}/config.ru")
+        # CONTRIBUTING.md
+      FileUtils.touch("#{app_name}/CONTRIBUTING.md")
+        # Gemfile
+      FileUtils.touch("#{app_name}/Gemfile")
+        # Rakefile
+      FileUtils.touch("#{app_name}/Rakefile")
+        # README.md
+      FileUtils.touch("#{app_name}/README.md")
     end
 
 end
